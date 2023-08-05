2c2c28309849b554c8ab6a01cd4da0b52c994ba9
mirkobrombin <send@mirko.pm>
2023-08-05 17:13:34 +0200

    use web hash history
    to make the router works on github pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import RecipeView from "../views/RecipeView.vue";
 import AtlasConfig from "@/config";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
